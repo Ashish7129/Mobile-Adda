514f92b438c1c13b36b977e0590ef6b99a532867
Ashish Aggarwal <ashish.aggarwal@nagarro.com>
2020-05-17 00:50:21 +0530

    add redux setup

---
+++ online-mobile-shop/src/components/Products/ProductContainer.js
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+function ProductContainer() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ProductContainer
---
+++ online-mobile-shop/src/redux/products/productActions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import {
+  FETCH_PRODUCTS_REQUEST,
+  FETCH_PRODUCTS_SUCCESS,
+  FETCH_PRODUCTS_FAILURE,
+} from "./productConstants";
+
+export const fetchProducts = () => {
+  return (dispatch) => {
+    dispatch(fetchProductsRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/products")
+      .then((response) => {
+        // response.data is the products
+        const products = response.data;
+        dispatch(fetchProductsSuccess(products));
+      })
+      .catch((error) => {
+        // error.message is the error message
+        dispatch(fetchProductsFailure(error.message));
+      });
+  };
+};
+
+export const fetchProductsRequest = () => {
+  return {
+    type: FETCH_PRODUCTS_REQUEST,
+  };
+};
+
+export const fetchProductsSuccess = (products) => {
+  return {
+    type: FETCH_PRODUCTS_SUCCESS,
+    payload: products,
+  };
+};
+
+export const fetchProductsFailure = (error) => {
+  return {
+    type: FETCH_PRODUCTS_FAILURE,
+    payload: error,
+  };
+};
