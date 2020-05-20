1004603db57f8dbc449935b48e9831f1dda52bce
Ashish Aggarwal <ashish.aggarwal@nagarro.com>
2020-05-21 00:32:03 +0530

    product detail page

--- online-mobile-shop/src/components/Products/ProductDetail.js
+++ online-mobile-shop/src/components/Products/ProductDetail.js
@@ -1,22 +1,55 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { connect } from "react-redux";
+import { fetchProducts } from "../../redux";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const mapStateToProps = (state) => {
+  return {
+    productData: state.products,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchProducts: () => dispatch(fetchProducts()),
+  };
+};
 
 class ProductDetailComponent extends Component {
   state = {
     loading: false,
     product: null,
     quantity: 1,
+    allProducts: [],
   };
-
   render() {
+    this.setState({ allProducts: this.props.productData.products });
     if (this.state.loading) {
       return <CircularProgress className="circular" />;
     }
-
-    return <div> Product Detail Page </div>;
+    let product = this.props.allProducts.filter(
+      (x) => x.id === this.props.match.params.id
+    );
+    return (
+      <div>
+        {" "}
+        {product.name}
+        {/* {this.props.productData.map((x) => (
+          <div>x.name</div>
+        ))} */}
+        {/* {product.name} {this.props.match.params.id} */}
+      </div>
+    );
   }
 }
 
-let ProductDetail = connect()(ProductDetailComponent);
+ProductDetailComponent.propTypes = {
+  fetchProducts: PropTypes.func.isRequired,
+};
+
+let ProductDetail = withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(ProductDetailComponent)
+);
 export default ProductDetail;
